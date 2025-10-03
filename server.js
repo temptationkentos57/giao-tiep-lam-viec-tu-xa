10937eed7f701f7337320b9e80e045c8801f2ae3
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-03 18:24:56 +0200

    Update MongoDB connection URI for better readability.

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/giao-tiep-lam-viec-tu-xa', { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/giao-tiep-lam-viec-tu-xa';
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Successfully connected to MongoDB!'))
   .catch(err => console.log(err));
 
