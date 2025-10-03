38037cd67e12dafcf0fb24a37411099b9c65533b
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-03 18:25:12 +0200

    Updated MongoDB connection URI for better clarity.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/giao-tiep-lam-viec-tu-xa';
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/remote-work-communication';
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Successfully connected to MongoDB!'))
   .catch(err => console.log(err));
