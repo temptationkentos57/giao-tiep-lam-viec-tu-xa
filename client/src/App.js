93918e78cd7bcdcc073f33c793a6cfbc5c46710b
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-03 18:23:11 +0200

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Giao Tiếp Làm Việc Từ Xa</h1>
+      <p>Chào mừng bạn đến với ứng dụng!</p>
+    </div>
+  );
+}
+
+export default App;
