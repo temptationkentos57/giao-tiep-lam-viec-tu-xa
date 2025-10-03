3cdb88d46154eec76452e028a8fa826c940cb4a7
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-03 18:23:46 +0200

    Updated MongoDB connection string to use environment variable for URI.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/giao-tiep-lam-viec-tu-xa', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/giao-tiep-lam-viec-tu-xa', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Kết nối đến MongoDB thành công!'))
   .catch(err => console.log(err));
 
